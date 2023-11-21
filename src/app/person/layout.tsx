fdbb1ffa34b13199c3c1678b51b6520785f7260d
iwenchen <iwenchen@tencent.com>
2023-11-21 17:44:48 +0800

    chore: input

---
+++ src/app/person/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Person',
+  description: 'A next app/person',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="zh-CN">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/person/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Personal() {
+  // const [count, setCount] = useState(10)
+  return <div>
+    {'Personal'}
+    {/* {count} */}
+    {/* <button onClick={() => setCount(v => v - 1)}>-</button> */}
+    {/* <button onClick={() => setCount(v => v + 1)}>+</button> */}
+  </div>
+}
